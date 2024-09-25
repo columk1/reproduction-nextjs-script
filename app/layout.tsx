c8f8d222f67633a234793fe81d788b456135e290
Colum Kelly <columk1@users.noreply.github.com>
2024-09-25 11:06:45 -0700

    Init script example

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,24 @@
+import Script from 'next/script'
+import './globals.css'
+
+const applyDarkMode = `
+  try {
+    document.documentElement.classList.add('dark')
+  } catch (_) {}
+`
+
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <head>
+        {/* This works */}
+        {/* <script dangerouslySetInnerHTML={{ __html: applyDarkMode }} /> */}
+      </head>
+      <body>
+        {/* This will cause a flash of unstyled content (throttle to fast 4G to see it)*/}
+        <Script strategy='beforeInteractive' dangerouslySetInnerHTML={{ __html: applyDarkMode }} />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
